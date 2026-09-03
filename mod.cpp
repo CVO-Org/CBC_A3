2f50891b3126fae763edc9fb72c54826e9e25033
Andx667 <andx667@protonmail.com>
2026-09-03 22:45:29 +0200

    add semi-temp images and rhai hook for version update

--- mod.cpp
+++ mod.cpp
@@ -3,13 +3,13 @@ name = "Community Base Compats {version}";
 
 author = "CBC Team";
 
-picture         = "img\flag_voron_red_1024_2048.paa";                                                                                               // Picture displayed from the expansions menu. Optimal size is 2048x1024
+picture         = "img\logo_ca.paa";                                                                                                                    // Picture displayed from the expansions menu. Optimal size is 2048x1024
 hideName        = "false";                                                                                                                          // Hide the extension name
 hidePicture     = "false";                                                                                                                          // Hide the extension menu
 
-logoSmall       = "img\Raven_Voron_64.paa";                                                                                                    // Display next to the item added by the mod 64x64
-logo            = "img\logo-voron-white_128.paa";                                                                                                   // Logo displayed in the main menu 128x128
-logoOver        = "img\logo-voron-red_128.paa";                                                                                                     // When the mouse is over, in the main menu 128x128
+logoSmall       = "img\logo_ca.paa";                                                                                                                // Display next to the item added by the mod 64x64
+logo            = "img\logo_ca.paa";                                                                                                                // Logo displayed in the main menu 128x128
+logoOver        = "img\logo_ca.paa";                                                                                                                // When the mouse is over, in the main menu 128x128
 
 tooltip         = "Community Base Compats";
 tooltipOwned    = "Community Base Addons Owned";                                                                                                          // Tool tip displayed when the mouse is left over, in the main menu
@@ -18,7 +18,7 @@ description     = "Bugtracker: https://github.com/CVO-Org/CBC_A3/issues";
 action          = "https://discord.gg/V37bRTnbb8/";                                                                                                // Website URL, that can accessed from the expansions menu 
 actionName      = "Discord";                                                                                                                        // label of button/tooltip in extension menu
 
-overviewPicture = "img\Raven_Voron_64.paa";
+overviewPicture = "img\logo_ca.paa";
 overviewText = "Community Base Compats overviewText";
 
 overviewFootnote = "<br /><br /><t color='#999999'>This content is under GPLv2 License.<br />Press <t /><t color='#19d3ff'>Left Shift + P<t /><t color='#999999'> to open the store page for more information.<t />";
